c866b0f1847fa93bece06ccfe1f9401424299381
noter28 <noterfield@gmail.com>
2018-10-22 20:51:53 +0300

    changed_data

--- rename_files.py
+++ rename_files.py
@@ -2,9 +2,9 @@
 import os, sys
 from PIL import Image #Python image library
 
-directory =         r'D:\\DONE_PHOTOS_FROM_FREELANCERS\20181019_done_photos\done_Dropbox\6.5_uah' #first source
+directory =         r'D:\\DONE_PHOTOS_FROM_FREELANCERS\20181022_links_for_upload' #first source
 outcome_directory = r'C:\\Users\descpc\Desktop\outcome_IC'     #output manually cropping photo
 files = os.listdir(directory) #list of item in directory
 for i  in files:
     img = Image.open(directory + '\\' + i)
-    img.save(outcome_directory + r'another' + (i))
+    img.save(outcome_directory + r'\1-1-' + (i))
