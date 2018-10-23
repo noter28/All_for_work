6af69ee72de573c16cb63f0c5d8fc2c67489e2d2
noter28 <noterfield@gmail.com>
2018-10-23 17:59:21 +0300

    just download links

--- open_links_from_csv.py
+++ open_links_from_csv.py
@@ -1,27 +1,26 @@
-# import re
-# result = re.search(r'Analytics', 'AV Analytics Vidhya AV')
-# print(result.group(0))
-# def qwe(x,b,c):
-#     q=x*b
-#     return q
-# def wer():
-#     p=qwe(10,10,10)+2
-#     return (p)
-# print(wer())
+
 # открываем все ссылки с csv файла
 import webbrowser
 import csv
+from urllib.request import urlopen
 
 with open('qwerty.csv') as f:
     reader = csv.reader(f)
-    for row in reader:
-        webbrowser.open(row[0], new=0, autoraise=True)
+    for link in reader:
+        resource = urlopen(link[0])
+        name = link[0].split('/')
+        out = open(r'D:\DONE_PHOTOS_FROM_FREELANCERS\20181023_done_photos' + r'/' + name[7], 'wb')
+        out.write(resource.read())
+        out.close()
+        text = name[7]
+        a = text.split('/')
+        print(a)
         # img = Image.open(directory + '\\' + (row[0]))
         # img.save(outcome_directory + '\\' + (row[0]))
         # os.remove(directory + '\\' + (row[0]))
 
 
-            #
+            #cmd
             #
             # for char in line:
             #     if char in " ?.!/;:":
