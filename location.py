08e00040e4f4a2dd7d3d0b7c7c86006d6d5478f9
noter28 <noterfield@gmail.com>
2018-11-02 16:55:57 +0200

    return needed part of json

--- location.py
+++ location.py
@@ -5,14 +5,18 @@ def loc():
     gmaps = googlemaps.Client(key='AIzaSyAv44kmc0VxYNSCh8CgZsEYPNnDEph8o3k')
 
 # Geocoding an address
-    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+    geocode_result = gmaps.geocode('520 Highway 119 Alabaster, AL 35007')
     a=geocode_result
+#    print(a)
     return(a)
 loc()
 # Look up an address with reverse geocoding
 #reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 b=json.dumps(loc())
+#pprint(b)
 n=json.loads(b)
-pprint(n)
-pprint(n['address_components'][0])
+a=(n[0])
+#pprint(a)
+print(a['geometry']['location'])
+# pprint(n['address_components'][0])
 
